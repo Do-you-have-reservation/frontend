ea922a9897ee521e4d899cd22a8a31c66877a047
jun <ggprgrkjh@naver.com>
2024-04-14 22:45:18 +0900

    feat(SlideSection): add bootstrap Modal reservation

--- src/components/SlideSection.tsx
+++ src/components/SlideSection.tsx
@@ -1,7 +1,10 @@
 import { addMachine, getReservations } from "../firebaseConfig";
+import "bootstrap/dist/css/bootstrap.min.css";
 import React, { useState, useEffect } from "react";
 import styled from "styled-components";
 import Countdown from "./Couontdown";
+import Modal from "react-bootstrap/Modal";
+import Button from "react-bootstrap/Button";
 
 const ScrollArea = styled.div`
   display: flex;
@@ -9,7 +12,7 @@ const ScrollArea = styled.div`
   overflow-x: auto;
   overflow-y: scroll;
   width: 100%;
-  height: 500px;
+  height: 1000px;
 `;
 
 const Item = styled.div`
@@ -17,10 +20,33 @@ const Item = styled.div`
   margin: 0 10px;
   background-color: #ccc;
 `;
-
-const Reservations = (props: any) => {
-  return <div>{props.name}</div>;
-};
+function MyVerticallyCenteredModal(props: any) {
+  return (
+    <Modal
+      {...props}
+      size="lg"
+      aria-labelledby="contained-modal-title-vcenter"
+      centered
+    >
+      <Modal.Header closeButton>
+        <Modal.Title id="contained-modal-title-vcenter">
+          Modal heading
+        </Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        <h4>Centered Modal</h4>
+        <p>
+          Cras mattis consectetur purus sit amet fermentum. Cras justo odio,
+          dapibus ac facilisis in, egestas eget quam. Morbi leo risus, porta ac
+          consectetur ac, vestibulum at eros.
+        </p>
+      </Modal.Body>
+      <Modal.Footer>
+        <Button onClick={props.onHide}>Close</Button>
+      </Modal.Footer>
+    </Modal>
+  );
+}
 
 function createItems() {
   const itemsArr = [];
@@ -32,7 +58,25 @@ function createItems() {
 
 const SlideSection = () => {
   const [items, setItems] = useState<any>([]);
-
+  const [modalShow, setModalShow] = React.useState(false);
+  const Reservations = (props: any) => {
+    return (
+      <button
+        style={{
+          width: "100%",
+          height: "140px",
+          backgroundColor: "skyblue",
+          marginTop: "5px",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+        onClick={() => setModalShow(true)}
+      >
+        {props.name}
+      </button>
+    );
+  };
   useEffect(() => {
     async function createItems() {
       const reservations = await getReservations();
@@ -44,28 +88,34 @@ const SlideSection = () => {
   }, [createItems]);
 
   return (
-    <ScrollArea>
-      {items.map((index: any) => {
-        return (
-          <Item key={index}>
-            <Countdown />
-            {index.reservations.map((reservation: string) => {
-              return <Reservations name={reservation}></Reservations>;
-            })}
-            <button onClick={() => getReservations()}>get</button>
-            <button onClick={() => addMachine()}>add</button>
-          </Item>
-        );
-      })}
-      <Item style={{ backgroundColor: "red" }}>
-        <button
-          onClick={() => addMachine()}
-          style={{ width: "100%", height: "100%" }}
-        >
-          추가하기
-        </button>
-      </Item>
-    </ScrollArea>
+    <>
+      <ScrollArea>
+        {items.map((index: any) => {
+          return (
+            <Item key={index}>
+              <Countdown />
+              {index.reservations.map((reservation: string) => {
+                return <Reservations name={reservation}></Reservations>;
+              })}
+              <button onClick={() => getReservations()}>get</button>
+              <button onClick={() => addMachine()}>add</button>
+            </Item>
+          );
+        })}
+        <Item style={{ backgroundColor: "red" }}>
+          <button
+            onClick={() => addMachine()}
+            style={{ width: "100%", height: "100%" }}
+          >
+            추가하기
+          </button>
+        </Item>
+      </ScrollArea>
+      <MyVerticallyCenteredModal
+        show={modalShow}
+        onHide={() => setModalShow(false)}
+      />
+    </>
   );
 };
 
